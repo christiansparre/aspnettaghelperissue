357ff5d9a1f217ef99f7e1f29141ee2ab6da81c5
Christian Sparre <christian@sparre.it>
2015-09-30 13:33:30 +0200

    Initial commit

---
+++ TagHelperTest/src/TagHelperTest/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace TagHelperTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TagHelperTest/src/TagHelperTest/DivValueTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperTest
+{
+    [TargetElement("div", Attributes = "sp-value")]
+    public class DivValueTagHelper : TagHelper
+    {
+        [HtmlAttributeName("sp-value")]
+        public string Value { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!string.IsNullOrWhiteSpace(Value))
+            {
+                output.Content.SetContent(Value);
+            }
+        }
+    }
+}
